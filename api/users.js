4156df7052382b42f833a769678f231e25c3a7ef
Kevin Ong <kevinongg@gmail.comm>
2025-09-08 22:23:45 -0400

    add login handler and helper function

--- api/users.js
+++ api/users.js
@@ -1,4 +1,4 @@
-import { createUser } from "#db/queries/users";
+import { createUser, getUserByEmailAndPassword } from "#db/queries/users";
 import requireBody from "#middleware/requireBody";
 import { createToken } from "#utils/jwt";
 import express from "express";
@@ -12,9 +12,19 @@ router
     async (req, res) => {
       const { firstName, lastName, email, password } = req.body;
       const user = await createUser(firstName, lastName, email, password);
-      console.log(user);
-      console.log({ id: user.id });
+      // console.log(user);
+      // console.log({ id: user.id });
       const token = createToken({ id: user.id });
       res.status(201).send(token);
     }
   );
+
+router
+  .route("/login")
+  .post(requireBody(["email", "password"]), async (req, res) => {
+    const { email, password } = req.body;
+    const user = await getUserByEmailAndPassword(email, password);
+    if (!user) return res.status(401).send("Invalid email and password");
+    const token = createToken({ id: user.id });
+    res.status(200).send(token);
+  });
