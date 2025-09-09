e7f1aca969368af931f630b5e7e7bda0b7bec69f
Kevin Ong <kevinongg@gmail.comm>
2025-09-08 21:44:31 -0400

    add register handler and helper function

--- api/users.js
+++ api/users.js
@@ -1,3 +1,20 @@
+import { createUser } from "#db/queries/users";
+import requireBody from "#middleware/requireBody";
+import { createToken } from "#utils/jwt";
 import express from "express";
 const router = express.Router();
 export default router;
+
+router
+  .route("/register")
+  .post(
+    requireBody(["firstName", "lastName", "email", "password"]),
+    async (req, res) => {
+      const { firstName, lastName, email, password } = req.body;
+      const user = await createUser(firstName, lastName, email, password);
+      console.log(user);
+      console.log({ id: user.id });
+      const token = createToken({ id: user.id });
+      res.status(201).send(token);
+    }
+  );
