bc7719d0fa4d3696d61cfd8e2af35c328c4487e4
Kevin Ong <kevinongg@gmail.comm>
2025-09-09 23:15:55 -0400

    add try catches to api handlers

--- api/books.js
+++ api/books.js
@@ -7,35 +7,51 @@ export default router;
 import requireUser from "#middleware/requireUser";
 import { getReservationsByUserIdAndBookId } from "#db/queries/reservations";
 
-router.route("/").get(async (req, res) => {
-  const books = await getAllBooks();
-  res.status(200).send(books);
+router.route("/").get(async (req, res, next) => {
+  try {
+    const books = await getAllBooks();
+    res.status(200).send(books);
+  } catch (error) {
+    return next(error);
+  }
 });
 
 router.param("id", async (req, res, next, id) => {
-  const book = await getBookById(id);
-  if (!book) return res.status(404).send("Book not found");
-  req.book = book;
+  try {
+    const book = await getBookById(id);
+    if (!book) return res.status(404).send("Book not found");
+    req.book = book;
 
-  if (req.user) {
-    const reservation = getReservationsByUserIdAndBookId(
-      req.user.id,
-      req.book.id
-    );
-    req.reservation = reservation;
+    if (req.user) {
+      const reservation = getReservationsByUserIdAndBookId(
+        req.user.id,
+        req.book.id
+      );
+      req.reservation = reservation;
+    }
+    next();
+  } catch (error) {
+    return next(error);
   }
-  next();
 });
 
-router.route("/:id").get(async (req, res) => {
-  res.status(200).send(req.book);
+router.route("/:id").get(async (req, res, next) => {
+  try {
+    res.status(200).send(req.book);
+  } catch (error) {
+    return next(error);
+  }
 });
 
-router.route("/:id/reservations").get(requireUser, async (req, res) => {
-  if (!req.user.id !== req.reservation.user_id) {
-    return res
-      .status(403)
-      .send("You are not authorized to see the reservations");
+router.route("/:id/reservations").get(requireUser, async (req, res, next) => {
+  try {
+    if (!req.user.id !== req.reservation.user_id) {
+      return res
+        .status(403)
+        .send("You are not authorized to see the reservations");
+    }
+    res.status(200).send(reservation);
+  } catch (error) {
+    return next(error);
   }
-  res.status(200).send(reservation);
 });
